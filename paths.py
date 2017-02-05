cb1133a063abab02968856024b8944c32dbafb52
yr4000 <y4000r@gmail.com>
2017-02-05 14:24:17 +0200

    created paths.py - saves variables with paths to tables

---
+++ paths.py
@@ -0,0 +1,28 @@
+'''
+    write the paths to tables on your local computer here
+'''
+
+#Yairs paths
+CLICKS_YAIR = "C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/clicks_train.csv"
+DOC_META_YAIR = "C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/documents_meta.csv"
+PROMOTED_CONTENT_YAIR = "C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/promoted_content.csv"
+DOC_TOPICS_YAIR = "C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/documents_topics.csv"
+EVENTS_YAIR = "C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/events.csv"
+DOC_CATEGORIES_YAIR = "C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/documents_categories.csv"
+DOC_ENTETIES_YAIR = "C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/documents_entities.csv"
+MAIN_TABLE_YAIR = "C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/fifth_merge_w_top_sim.csv"
+
+
+#Deans paths
+CLICKS_DEAN = ""
+DOC_META_DEAN = ""
+PROMOTED_CONTENT_DEAN = ""
+DOC_TOPICS_DEAN = ""
+EVENTS_DEAN = ""
+DOC_CATEGORIES_DEAN = ""
+DOC_ENTETIES_DEAN = ""
+MAIN_TABLE_DEAN = ""
+
+
+
+#Erans Paths
