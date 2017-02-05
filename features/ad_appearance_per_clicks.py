65f5db73627c771a58eb4ee77ffff5188b62cd7b
yr4000 <y4000r@gmail.com>
2017-02-05 16:27:53 +0200

    modified ad_appearance_per_clicks to work with relevant ids and to be a function

--- features/ad_appearance_per_clicks.py
+++ features/ad_appearance_per_clicks.py
@@ -1,35 +1,44 @@
+from utils.plot_utils import *
+from utils.table_utils import *
+from paths import *
 import numpy as np
 import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from utils.plot_utils import *
 
 
-#how many times an ad appeared:
-adAppearance = pd.read_csv("C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/clicks_train.csv",
-                     nrows=1000000, usecols=["ad_id"])
+def create_ad_appearance(main_table):
+    #how many times an ad appeared:
+    clicks = pd.read_csv(CLICKS_YAIR, usecols=['ad_id','clicked'])
+    relevant_ads = return_unique_values_of_column_from_table('ad_id',main_table)
+    filtered_clicks = filter_table_by_unique_ids(relevant_ads,'ad_id',clicks)
 
-adAppearance["appearance"] = adAppearance["ad_id"]
-adAppearance = adAppearance.groupby(["ad_id"],as_index=False).agg({"appearance":np.count_nonzero})
+    adAppearance = filtered_clicks.drop(['clicked'],axis=1)
+    adAppearance["appearance"] = adAppearance["ad_id"]
+    adAppearance = adAppearance.groupby(["ad_id"],as_index=False).agg({"appearance":np.count_nonzero})
 
-adArr = np.array(adAppearance["ad_id"])
-aprArr = np.array(adAppearance["appearance"])
+    '''
+    adArr = np.array(adAppearance["ad_id"])
+    aprArr = np.array(adAppearance["appearance"])
 
-create_simple_histogram(adArr,aprArr,'Number of time ad appearded',"ad_ID","Appearances","ad_appearance.png")
+    create_simple_histogram(adArr,aprArr,'Number of time ad appearded',"ad_ID","Appearances","ad_appearance.png")
+    '''
 
+    #how many times an ad got a click
 
-#how many times an ad got a click
+    adClicks = filtered_clicks.groupby(["ad_id"],as_index=False).agg({"clicked":np.sum})
+    adAppearance = adAppearance.merge(adClicks,on = "ad_id")
+    adAppearance['clicks_per_appearance'] = adAppearance['clicked']/adAppearance['appearance']
+    adAppearance.drop(adAppearance.columns[[1,2]],axis=1,inplace=True)
+    return adAppearance
 
-adClicks = pd.read_csv("C:/Users/Yair/Desktop/Data Science Workshop/Outbrain Competition/clicks_train.csv",
-                     nrows=1000000, usecols=["ad_id","clicked"])
-adClicks = adClicks.groupby(["ad_id"],as_index=False).agg({"clicked":np.sum})
+'''
 adArr = np.array(adClicks["ad_id"])
 clicksArr = np.array(adClicks[["clicked"]])
 
 #TODO from some reason on Yairs computer runs get stuck, be carful if you ren this.
 create_simple_histogram(adArr,clicksArr,'Number of time ad got clicked',"Ad_ID","Clicks","clicks_per_ad.png")
-
 
 #a plot which shows per ad_id: #appearance - (#appearances-#clicks) assume #appearnces>=#clicks
 
@@ -39,16 +48,5 @@ for i in range(len(clicksArr)):
 aprClkArr = np.array(aprClkArr)
 
 create_simple_histogram(adArr,aprClkArr,'Clicks/Appearances per ad',"Ad_ID","Clicks/Appearances","Clicks_Appearances_per_ad.png")
-
-'''
-sum0 = 0
-for x in aprClkArr:
-    if x==0:
-        sum0 += 1
-
-sum1 = 0
-for x in aprClkArr:
-    if x==1:
-        sum1 += 1
 '''
 
