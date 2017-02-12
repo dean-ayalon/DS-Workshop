9186a330a89bece8e5d405f9ec991b205f93f738
Dean Ayalon <ayalondean@gmail.com>
2017-02-12 17:59:01 +0200

    Added "Ad Count Per Display" Feature

---
+++ features/ad_count_per_display.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+
+def ad_count_per_display(main_table):
+    # Extracting the display_id's vector from main table
+    displays_and_ads = main_table[["display_id"]]
+
+    # Counting how many times every display_id appears in the vector - this equlas the ad count per each display
+    ad_counts = displays_and_ads.groupby("display_id").display_id.agg("count")
+    ad_counts = pd.Series(ad_counts.as_matrix())
+
+    # Extracting vector of unique displays
+    displays = displays_and_ads["display_id"].unique()
+
+    # Creating result Dataframe, contaning the ad count for each display_id
+    # (has only display_id and ad_count columns)
+    res_frame = pd.DataFrame()
+    res_frame["display_id"] = displays
+    res_frame["ad_count_per_display"] = ad_counts
+
+    return res_frame
