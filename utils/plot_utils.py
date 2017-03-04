3c805c4f7295d19fea361f61d65dc70cbc861c1c
yr4000 <y4000r@gmail.com>
2017-03-04 14:37:39 +0200

    added pie chart to display size feature.

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -5,7 +5,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from paths import *
 
-main_table = pd.read_csv(MAIN_TABLE_YAIR)
+#main_table = pd.read_csv(MAIN_TABLE_YAIR)
 
 def create_simple_histogram(categoryArr,countArr,title,x_label,y_label,file_name):
     fig = plt.figure()
@@ -20,22 +20,37 @@ def create_simple_histogram(categoryArr,countArr,title,x_label,y_label,file_name
 def plot_platform_histogram(main_table):
     platforms_count = main_table[["platform_is_desktop","platform_is_mobile","platform_is_tablet"]]
     platforms_count = np.array(platforms_count.sum())
-    #platforms_names = np.array(["Desktop","Mobile","Tablet"])
-    platforms_names = np.array([0,1,2])
+    platforms_names = np.array(["Desktop","Mobile","Tablet"])
     fig = plt.figure()
     fig.suptitle("Amount of platforms",fontsize=14, fontweight='bold')
     ax = fig.add_subplot(111)
     ax.set_xlabel("Platform type")
     ax.set_ylabel("Amount")
-    '''
     width = 0.3
-    bins = map(lambda x: x - width / 2, range(1, len(platforms_count) + 1))
+    bins = list(map(lambda x: x - width / 2, range(1, len(platforms_count) + 1)))
     ax.bar(bins, platforms_count, width=width)
-    #ax.set_xticks(map(lambda x: x, range(1, len(data) + 1)))
+    ax.set_xticks(list(map(lambda x: x, range(1, len(platforms_count) + 1))))
     ax.set_xticklabels(platforms_names)
-    '''
 
-    plt.bar(left=platforms_names, height=platforms_count)
+    #plt.bar(left=platforms_names, height=platforms_count)
+    #plt.savefig("test.png")
     #plt.show()
 
+def create_disp_number_piechart(main_table):
+    disp_size = main_table[["display_id","ad_id"]]
+    disp_size = disp_size.groupby(["display_id"],as_index=False).agg({"ad_id":np.count_nonzero})\
+        .rename(index=str, columns={"ad_id" : 'disp_size'})
+    disp_size_count = disp_size.groupby(["disp_size"],as_index=False).agg({"display_id":np.count_nonzero})\
+        .rename(index=str, columns={"display_id" : 'disp_size_count'})
+    disp_size_count_arr = np.array(disp_size_count['disp_size_count'])
+    disp_size_type = np.array(disp_size_count['disp_size'])
+
+    #now create the pie chart
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111)
+    fig1, ax1 = plt.subplots()
+    ax1.pie(disp_size_count_arr, labels=disp_size_type, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')
+    #plt.savefig('test.png')
 
